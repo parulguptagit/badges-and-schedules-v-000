a7f35370ad5836bf405a5fbf0b0430351393944d
Parul Gupta <parulguptasmail@gmail.com>
2018-03-07 19:32:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,18 @@
 def badge_maker(name)
   "Hello, my name is Arel."
 end
+
+def batch_badge_creator(speaker_list)
+  message_list = []
+  speaker_list.each do |speaker|
+    message_list << badge_maker(speaker)
+  end
+  message_list
+end
+
+def assign_rooms(speaker_list)
+  room_list = []
+  speaker_list.each do |speaker|
+    room_list << "Hello, #{speaker}! You'll be assigned to room #{speaker.index}"
+  end
+end
